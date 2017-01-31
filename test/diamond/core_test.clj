032b364c2b7c87e782a7c817d8d5178f29da1a6d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-31 23:43:27 +0100

    add 'produces-the-expected-number-of-lines' spec

--- test/diamond/core_test.clj
+++ test/diamond/core_test.clj
@@ -7,12 +7,18 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
-(defn upper-case-chars []
+(def upper-case-chars
   (let [A (int \A)
         Z (int \Z)]
     (map char (range A Z))))
 
 (defspec produces-some-lines
          100
-         (prop/for-all [v (gen/elements (upper-case-chars))]
+         (prop/for-all [v (gen/elements upper-case-chars)]
                        (> (count (create v)) 0)))
+
+(defspec produces-the-expected-number-of-lines
+         100
+         (prop/for-all [v (gen/elements upper-case-chars)]
+                       (= (count (create v))
+                          (+ 1 (* 2 (- (int v) (int \A)))))))
