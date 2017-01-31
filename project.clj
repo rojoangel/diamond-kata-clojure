b85945039104130ee9b9e5a87c261e13859e403e
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-31 23:10:57 +0100

    add lein-test-refresh

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "Diamond Kata in Clojure using Property Based Testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot diamond.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]]}})
