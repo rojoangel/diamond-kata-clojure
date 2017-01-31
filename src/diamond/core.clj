0dd4aaab5f0a58dec27bbc03fcc55fe48d6a4cbf
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-31 23:49:23 +0100

    refactor by extracting 'char->int' and 'line-count' functions

--- src/diamond/core.clj
+++ src/diamond/core.clj
@@ -1,5 +1,11 @@
 (ns diamond.core
   (:gen-class))
 
+(defn- char->int [ch]
+  (- (int ch) (int \A)))
+
+(defn- line-count [ch]
+  (inc (* 2 (char->int ch))))
+
 (defn create [ch]
-  (repeat (+ 1 (* 2 (- (int ch) (int \A)))) "A"))
+  (repeat (line-count ch) "A"))
