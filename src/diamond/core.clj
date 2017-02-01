a70f273af39997773e0e088351e926fc4de08896
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-01 01:52:25 +0100

    make 'is-vertically-symmetrical' pass by processing separately top and bottom halves

--- src/diamond/core.clj
+++ src/diamond/core.clj
@@ -13,4 +13,6 @@
   (inc (* 2 (char->int ch))))
 
 (defn create [ch]
-  (take (square-side ch) (map (partial repeat (square-side ch)) (cycle upper-case-chars))))
+  (let [top-half (take (inc (char->int ch)) (map (partial repeat (square-side ch)) upper-case-chars))
+        bottom-half (reverse (take (dec (count top-half)) top-half))]
+    (concat top-half bottom-half)))
