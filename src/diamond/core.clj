50f6fdf764195e68ef444963960313d2868642b6
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-01 23:14:52 +0100

    make all tests pass by calculating the top-right-half in a convoluted way

--- src/diamond/core.clj
+++ src/diamond/core.clj
@@ -13,6 +13,10 @@
   (inc (* 2 (char->int ch))))
 
 (defn create [ch]
-  (let [top-half (take (inc (char->int ch)) (map (partial repeat (square-side ch)) upper-case-chars))
+  (let [top-right-half (take (inc (char->int ch)) (map (fn [line-char] (concat
+                                                                         (repeat (char->int line-char) \space)
+                                                                         (cons line-char nil)
+                                                                         (repeat (- (quot (square-side ch) 2) (char->int line-char)) \space))) upper-case-chars))
+        top-half (map (fn [line] (concat (reverse line) (drop 1 line))) top-right-half)
         bottom-half (reverse (take (dec (count top-half)) top-half))]
     (concat top-half bottom-half)))
