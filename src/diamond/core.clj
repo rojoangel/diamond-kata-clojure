f001c65bf3f126ad4b813caccd9f247d1cd6cad8
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-02 20:08:31 +0100

    refactor 'create' to use a different approach and fill the line with sequence of different characters instead of all the same - last test failing

--- src/diamond/core.clj
+++ src/diamond/core.clj
@@ -12,11 +12,15 @@
 (defn square-side [ch]
   (inc (* 2 (char->int ch))))
 
+(defn half-square-side [ch]
+  (/ (inc (square-side ch)) 2))
+
+(defn half-square-row [ch]
+  (take (half-square-side ch) upper-case-chars))
+
+(defn square-row [ch]
+  (let [half-row (half-square-row ch)]
+    (concat (reverse (drop 1 half-row)) half-row)))
+
 (defn create [ch]
-  (let [top-right-half (take (inc (char->int ch)) (map (fn [line-char] (concat
-                                                                         (repeat (char->int line-char) \space)
-                                                                         (cons line-char nil)
-                                                                         (repeat (- (quot (square-side ch) 2) (char->int line-char)) \space))) upper-case-chars))
-        top-half (map (fn [line] (concat (reverse line) (drop 1 line))) top-right-half)
-        bottom-half (reverse (take (dec (count top-half)) top-half))]
-    (concat top-half bottom-half)))
+  (repeat (square-side ch) (square-row ch)))
