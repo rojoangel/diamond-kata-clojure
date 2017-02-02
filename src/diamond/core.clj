69bfac38183cef42115def2df5b7835fc3f797c0
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-02 21:16:19 +0100

    make all tests pass by combining 'mirror', 'reverse' and 'replace-other-chars-with-spaces'

--- src/diamond/core.clj
+++ src/diamond/core.clj
@@ -15,10 +15,19 @@
 (defn- mirror [coll]
   (concat (reverse (drop 1 coll)) coll))
 
-(defn square-row [ch]
-  (let [half-square-side (/ (inc (square-side ch)) 2)
+(defn- half-square-side [ch]
+  (quot (inc (square-side ch)) 2))
+
+(defn- square-row [ch]
+  (let [half-square-side (half-square-side ch)
         half-square-row (take half-square-side upper-case-chars)]
     (mirror half-square-row)))
 
+(defn- replace-other-chars-with-spaces [ch row]
+  (map #(if (= ch %) ch \space) row))
+
 (defn create [ch]
-  (repeat (square-side ch) (square-row ch)))
+  (mirror
+    (reverse
+      (map replace-other-chars-with-spaces upper-case-chars
+           (repeat (half-square-side ch) (square-row ch))))))
