704ff9638cf982184eb8657bb78afaa438b9e41c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-12 16:39:48 +0100

    create new empty 'master' branch

--- src/diamond/io.clj
+++
@@ -1,6 +0,0 @@
-(ns diamond.io
-  (:require [diamond.core :refer :all]
-            [clojure.string :as str]))
-
-(defn print [diamond]
-  (println (str/replace (str/join "\n" (map #(apply str %) diamond)) #" " "-")))
