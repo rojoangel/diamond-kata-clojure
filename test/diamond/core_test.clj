7ebc932b115cc13a1c4ca197a06b519df76dd3ee
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-12 12:48:14 +0100

    add failing 'each-row-contains-the-right-character' spec and supporting 'first-half' and 'int->char' functions to verify the right character is present at each row

--- test/diamond/core_test.clj
+++ test/diamond/core_test.clj
@@ -11,6 +11,14 @@
         Z (int \Z)]
     (map char (range A Z))))
 
+(defn- first-half [diamond]
+  (let [size (inc (quot (count diamond) 2))]
+    (take size diamond)))
+
+(defn- int->char [idx]
+  (let [A-idx (int \A)]
+    (char (+ idx A-idx))))
+
 (defspec produces-a-square                                  ;; the name of the test
          100                                                ;; the number of iterations for test.check to test
          (prop/for-all [char (gen/elements (upper-case-chars))]
@@ -24,3 +32,13 @@
                        (let [diamond (create char)
                              square-side (square-side char)]
                          (= (count diamond) square-side))))
+
+;; start leveraging horizontal symmetry
+(defspec each-row-contains-the-right-character              ;; the name of the test
+         100                                                ;; the number of iterations for test.check to test
+         (prop/for-all [char (gen/elements (upper-case-chars))]
+                       (let [diamond (create char)
+                             top-diamond (first-half diamond)]
+                         (every?
+                           (fn [[idx line]] (some #(= (int->char idx) %) line))
+                           (map-indexed vector top-diamond)))))
