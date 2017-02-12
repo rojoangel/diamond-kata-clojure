010debd89708fa9b424accd248ca294b73d31808
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-12 11:32:21 +0100

    add failing 'produces-some-lines' spec to check setup

--- test/diamond/core_test.clj
+++ test/diamond/core_test.clj
@@ -1,3 +1,12 @@
 (ns diamond.core-test
   (:require [clojure.test :refer :all]
-            [diamond.core :refer :all]))
+            [diamond.core :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.string :as str]))
+
+(defspec produces-some-lines                                ;; the name of the test
+         100                                                ;; the number of iterations for test.check to test
+         (prop/for-all [char (gen/fmap str/upper-case gen/char-alpha)]
+                       (> (count (create char)) 0)))
